18fe06a11c194e9b7564a3598ceb9f1ee4d99e39
ClemEshn <contact.klems@gmail.com>
2025-03-30 15:50:50 +0200

    Page working, mission 1 complete

--- Views/ClementsNewPage.php
+++ Views/ClementsNewPage.php
@@ -1,3 +1,34 @@
+<?php
+
+/*Pistes d'améliorations : 
+    - Utilisation de messages type "toast"
+    - Vérifications plus profondes du contenu saisi par l'utilisateur
+*/
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cpf_nonce']) && wp_verify_nonce($_POST['cpf_nonce'], 'cpf_submit_form')) {
+    $title   = sanitize_text_field($_POST['cpf_title']);
+    $content = wp_kses_post($_POST['cpf_content']);
+    $meta    = sanitize_text_field($_POST['cpf_meta']);
+
+    if(strlen($title)  !== 0|| strlen($content)  !== 0 || strlen($meta)  !== 0){
+        $post_id = wp_insert_post([
+            'post_title'   => $title,
+            'post_content' => $content,
+            'post_status'  => 'publish',
+            'post_type'    => 'post',
+        ]);
+    }
+
+    if ($post_id && !is_wp_error($post_id)) {
+        add_post_meta($post_id, 'mymeta', $meta);
+        echo '<div class="notice notice-success">Article publié avec succès !</div>';
+    } else {
+        echo '<div class="notice notice-error">Erreur lors de la publication, verifiez les informations saisies.</div>';
+    }
+}
+?>
+
+
 <h2 style="text-align: center;">Mon formulaire pour créer un post et ses metadata</h2>
 
 <form method="post" style="max-width: 500px; margin: 0 auto;">
@@ -42,6 +73,9 @@
     border-radius: 8px; 
     background: transparent; 
     cursor: pointer;
+}
+.wp-block-post-title{
+    display : none;
 }
 
 </style>
